6825f702aa5e80afbc27da94dbb2ccd049b09e84
Austin Zimmer <zimmeren@umich.edu>
2014-12-12 14:58:47 -0500

    Added functionality and screen layout to add schism screen

--- Schism/NewSchismViewController.swift
+++ Schism/NewSchismViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class NewSchismViewController: UIViewController {
+class NewSchismViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.myLeftTextField.delegate = self
+        self.myRightTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +22,52 @@ class NewSchismViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        self.view.endEditing(true);
+        return false;
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true);
+    }
 
+    @IBOutlet weak var myLeftTextField: UITextField!
+    
+    @IBOutlet weak var myRightTextField: UITextField!
+    
+    @IBOutlet weak var postButton: UIButton!
+    
+    @IBAction func postButtonPress(sender: AnyObject) {
+        println("posting bet")
+        var left: String = ""
+        var right: String = ""
+        var valid: Bool = true
+        if myLeftTextField.text != ""{
+            left = myLeftTextField.text
+        }
+        else{
+            valid = false
+        }
+        if myRightTextField.text != ""{
+            right = myRightTextField.text
+        }
+        else{
+            valid = false
+        }
+        if valid{
+            println("uid: \(data.uid) lat: \(data.lat) lon: \(data.lon) leftTerm: \(left) rightTerm: \(right)")
+            var posted : Bool = network.post(data.uid, lat: data.lat, lon: data.lon, leftTerm: left, rightTerm: right)
+            if (posted){
+                self.navigationController?.popViewControllerAnimated(true)
+            }
+            else{
+                println("network error posting this schism")
+            }
+        }
+        else{
+            println("invalid schism: Please enter Text")
+        }
+    }
     /*
     // MARK: - Navigation
 
